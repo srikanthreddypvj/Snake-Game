ac4e44438a1c28522541487d67cbd44ca3e5241c
Panduga Venkata Jaya Srikanth Reddy <94885124+srikanthreddypvj@users.noreply.github.com>
2025-03-19 07:35:40 +0530

    Update input.js

--- input.js
+++ input.js
@@ -1,6 +1,6 @@
 let inputDirection = { x: 0, y: 0 }
 let lastInputDirection = { x: 0, y: 0 }
-//////////////
+/////////////
 window.addEventListener('keydown', e => {
   switch (e.key) {
     case 'ArrowUp':
